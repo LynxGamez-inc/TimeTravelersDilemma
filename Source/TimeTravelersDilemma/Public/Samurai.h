d8af9292adfdfa345082ca853e60f7d22198d1a9
mobinabiuk <mobinboyok@gmail.com>
2025-03-11 16:15:02 +0300

    Creating Samurai Class

---
+++ Source/TimeTravelersDilemma/Public/Samurai.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Samurai.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMETRAVELERSDILEMMA_API ASamurai : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
